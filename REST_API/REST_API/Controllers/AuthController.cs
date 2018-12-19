aa5a29cc5ff8c946a4b734bcc690b24d424c2d97
Matouš Kulhan <kulhanmatous@sssvt.cz>
2018-12-19 21:33:44 +0100

    Mysql.Data package + AuthController

--- REST_API/REST_API/Controllers/AuthController.cs
+++ REST_API/REST_API/Controllers/AuthController.cs
@@ -4,10 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using REST_API.Models.Api;
+using REST_API.Models.Api.Auth;
 
 namespace REST_API.Controllers
 {
     public class AuthController : ApiController
     {
+        public Response Login([FromBody]UserCredentials user)
+        {
+            return new Response();
+        }
+
+        public Response Register([FromBody]UserCredentials user)
+        {
+            return new Response();
+        }
     }
 }
---
+++ REST_API/REST_API/Models/Api/Auth/UserCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST_API.Models.Api.Auth
+{
+    public class UserCredentials
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
