eb0320843234fcf5523f4040fa61f8aa51e5740b
Matouš Kulhan <m77m77@seznam.cz>
2018-12-21 15:36:23 +0100

    GroupInfo

--- REST_API/REST_API/Repositories/GroupRepository.cs
+++ REST_API/REST_API/Repositories/GroupRepository.cs
@@ -27,7 +27,7 @@ namespace REST_API.Repositories
 
         public List<GroupInfo> FindAllForUser(uint userId)
         {
-            string sql = "SELECT g.Id,IF(g.GroupName IS NOT NULL,g.GroupName,(SELECT GROUP_CONCAT(IF(gu.Nickname IS NULL, uu.Name, gu.Nickname) SEPARATOR ', ') FROM Group_User gu INNER JOIN User uu ON uu.Id = gu.Id_User WHERE gu.Id_Group = g.Id)) AS Name, g.Id_Attachment,IF(senderG.Nickname IS NULL, senderU.Name, senderG.Nickname) AS sender, m.TextBody,m.Sent " +
+            string sql = "SELECT g.Id,IF(g.GroupName IS NOT NULL,g.GroupName,(SELECT GROUP_CONCAT(IF(gu.Nickname IS NULL, uu.Name, gu.Nickname) SEPARATOR ', ') FROM Group_User gu INNER JOIN User uu ON uu.Id = gu.Id_User WHERE gu.Id_Group = g.Id AND gu.Id_User != @uid)) AS Name, g.Id_Attachment,u.IgnoreExpire,IF(senderG.Nickname IS NULL, senderU.Name, senderG.Nickname) AS sender, m.TextBody,m.Sent,(SELECT COUNT(mn1.Id) FROM Message mn1 WHERE mn1.Id_Group = g.Id) - (SELECT COUNT(mn2.Id) FROM Message mn2 INNER JOIN MessageState ms ON mn2.Id = ms.Id_Message WHERE mn2.Id_Group = g.Id AND ms.Id_User = @uid AND Seen = 1) AS NewMessages " +
                          "FROM `Group` g INNER JOIN Group_User u ON g.Id = u.Id_Group LEFT JOIN (SELECT gd.Id, MAX(m.Id) as mId FROM `Group` gd LEFT JOIN Message m ON m.Id_Group = gd.Id GROUP BY gd.Id) a ON a.Id = g.Id LEFT JOIN Message m ON a.mId = m.Id LEFT JOIN Group_User senderG ON senderG.Id_User = m.Id_User AND senderG.Id_Group = g.ID LEFT JOIN User senderU ON senderU.Id = m.Id_User WHERE u.Id_User = @uid ORDER BY a.mId DESC";
 
             return this.ReadToListGroupInfo(this.db.ExecuteReader(sql, new Dictionary<string, object>() { { "uid", userId } }));
@@ -44,11 +44,13 @@ namespace REST_API.Repositories
                 GroupInfo group = new GroupInfo()
                 {
                     Id = reader.GetUInt32("Id"),
-                    Name = reader.GetString("Name"),
+                    Name = reader.IsDBNull(reader.GetOrdinal("Name")) ? "Pouze já" : reader.GetString("Name"),
                     Id_Attachment = reader.GetUInt32("Id_Attachment"),
+                    IgnoreExpire = reader.IsDBNull(reader.GetOrdinal("IgnoreExpire")) ? (DateTime?)null : reader.GetDateTime("IgnoreExpire"),
                     LastMessageSender = reader.IsDBNull(reader.GetOrdinal("sender")) ? null : reader.GetString("sender"),
                     LastMessageText = reader.IsDBNull(reader.GetOrdinal("TextBody")) ? null : reader.GetString("TextBody"),
-                    LastMessageDate = reader.IsDBNull(reader.GetOrdinal("Sent")) ? (DateTime?) null : reader.GetDateTime("Sent")
+                    LastMessageDate = reader.IsDBNull(reader.GetOrdinal("Sent")) ? (DateTime?)null : reader.GetDateTime("Sent"),
+                    NewMessages = reader.GetInt32("NewMessages")
                 };
 
                 result.Add(group);
