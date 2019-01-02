90abd4f7cc347026a3c66416f89e9ac8cd8df206
MatejFrnka <mat.frnka@gmail.com>
2019-01-02 20:41:15 +0100

    Enabled cross Cross-origin resource sharing

--- REST_API/REST_API/App_Start/WebApiConfig.cs
+++ REST_API/REST_API/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace REST_API
 {
@@ -21,6 +22,9 @@ namespace REST_API
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             config.Formatters.JsonFormatter.SerializerSettings.SerializationBinder = JsonSerializationUtility.jsonSettings.SerializationBinder;
