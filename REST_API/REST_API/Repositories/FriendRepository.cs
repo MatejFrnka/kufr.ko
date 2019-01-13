5a03c7ac7b16e52ef223663c6600883eb18bdfd5
Matěj Kocourek <matej@kocourek.cz>
2019-01-13 10:44:36 +0100

    Default group

--- REST_API/REST_API/Repositories/FriendRepository.cs
+++ REST_API/REST_API/Repositories/FriendRepository.cs
@@ -59,7 +59,8 @@ namespace REST_API.Repositories
                     Id = reader.GetUInt32("Id"),
                     Name = reader.GetString("Name"),
                     Id_Attachment = reader.GetUInt32("Id_Attachment"),
-                    LastOnline = reader.IsDBNull(reader.GetOrdinal("LastOnline")) ? (DateTime?) null : reader.GetDateTime("LastOnline")
+                    LastOnline = reader.IsDBNull(reader.GetOrdinal("LastOnline")) ? (DateTime?) null : reader.GetDateTime("LastOnline"),
+                    DefaultGroup = reader.GetUInt32("GroupId")
                 };
                 result.Add(u);
             }
